3c4cca3778aa20efc217495f8748bcfcca2c95d6
AnikethKMenon <anikethkmenonbtech24@rvu.edu.in>
2026-02-19 12:05:10 +0530

    Update naivebayes.py

--- naivebayes.py
+++ naivebayes.py
@@ -1,16 +1,10 @@
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
-import seaborn as sns
-df = pd.read_csv("NBD.csv")
-df.head()
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=42)
-x=df.drop('diabetes',axis=1)
-y=df['diabetes']
-model=GaussianNB()
-model.fit(x_train,y_train)
-y_pred = model.predict(x_test)
-y_pred
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+X = [[30],[40],[50],[60],[20],[10],[70]]
+y = [0,1,1,1,0,0,1]
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(X,y)
+X_marks=[[20]]
+print(classifier.predict(X_marks))
